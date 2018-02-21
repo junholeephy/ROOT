0e865f4909560ae9f5798c3d72a9ccf6e40ac648
ico1036 <jiwoong.kim@cern.ch>
2018-02-22 00:22:09 +0900

    second commit

--- makeHist.C
+++ makeHist.C
@@ -1,12 +1,11 @@
 void makeHist() {
 
-	TFile *f1 = new TFile("out.root","create");	
 
 	TH1F* h1 = new TH1F("h1","h1",1000,-5,5);
 	h1->FillRandom("gaus",10000);
 
+	h1->Draw();
 
-	f1->Write();
 
 }
 
